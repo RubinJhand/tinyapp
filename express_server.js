9734df63af6774abb510acd5a408772f364424bb
Rubin <rjhand@gmail.com>
2020-06-08 23:00:27 +0000

    Added new route handler for /urls

--- express_server.js
+++ express_server.js
@@ -13,6 +13,13 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+app.get("/urls", (req, res) => {
+  let templateVars = {
+    urlDatabase
+  };
+  res.render("urls_index", templateVars);
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
