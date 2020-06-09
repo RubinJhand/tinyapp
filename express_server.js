54bfac11c0bf8bf199bcca903823aead218d89e0
Rubin <rjhand@gmail.com>
2020-06-09 14:32:48 +0000

    Added POST shortURL and longURL redirection: too many redirects; altered  generateRandomString

--- express_server.js
+++ express_server.js
@@ -8,10 +8,12 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
 const generateRandomString = () => {
-  //Google search led to this:
-  //http://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript
-  //not ideal for real world, but for my purposes, should work
- Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+  //Google search led to this: NOT MY IDEA (I did not come up with this)
+  //Source:
+    //http://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript
+
+  //apparently not ideal for real world, but for my purposes, should work; change (2, 15) to (2, 5);
+ return Math.random().toString(36).substring(2, 5) + Math.random().toString(36).substring(2, 5).toUpperCase();
 };
 
 
@@ -34,8 +36,15 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok'
+  // Log the POST request body to the console; output: { longURL: 'www.example.com' }
+  console.log(req.body);
+  //generate shortURL
+  const genShortURL = generateRandomString();
+  const longURL = req.body.longURL;
+  //Adds to urlDatabase
+  urlDatabase[genShortURL] = longURL;
+  //redirects to new shortURL
+  res.redirect(`/urls/${genShortURL}`);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
@@ -47,6 +56,13 @@ app.get("/urls/:shortURL", (req, res) => {
     longURL
   };
   res.render("urls_show", templateVars);
+});
+//redirects any request to /u/:shortURL to its longURL
+app.get("/u/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  const longURL = urlDatabase[shortURL];
+
+  res.redirect(longURL);
 });
 
 app.get("/urls.json", (req, res) => {
