ee4438d68545d6fca5b79e124a2ca9aa7044e145
Rubin <rjhand@gmail.com>
2020-06-09 03:25:14 +0000

    Added route for POST method

--- express_server.js
+++ express_server.js
@@ -24,6 +24,11 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok'
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   const longURL = urlDatabase[shortURL];
