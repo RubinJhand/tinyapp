934816a4e6d80981593a36e23d1550c1b68ef30c
Rubin <rjhand@gmail.com>
2020-06-14 03:02:07 +0000

    Added or modified some functions to DRY and debug

--- express_server.js
+++ express_server.js
@@ -1,5 +1,5 @@
-const { bodyParser, bcrypt, morgan, cookieSession, app, PORT, urlDatabase, users } = require('./dependentFunctions');
-const { getUserByEmail, generateRandomString, urlsForUser, renderTemplateVariables, eradicateCookies, creatCookie, reqContainer } = require('./helpers')
+const { bodyParser, bcrypt, morgan, cookieSession, app, PORT, urlDatabase, users } = require('./constRequires');
+const { getUserByEmail, generateRandomString, urlsForUser, renderTemplateVariables, eradicateCookies, creatCookie, emailPasswordReq, shortLongURLReq, loginCheck } = require('./helpers')
 
 app.set('view engine', 'ejs');
 
@@ -11,29 +11,36 @@ app.use(cookieSession( {
 }));
 
 app.get('/', (req, res) => {
-  const { userID } = reqContainer(req);
-  if (userID) {
+  if (loginCheck(req)) {
    res.redirect('/urls'); 
   } else {
-    res.redirect('login');
-  }
+    res.redirect('/login');
+  };
 });
 
 app.get('/urls', (req, res) => {
+  if (!loginCheck(req)) {
+    res.redirect('/login');
+  } else {
   renderTemplateVariables(req, res, urlsForUser(req.session.user_id, urlDatabase), users, 'urls_index');
+  };
 });
 
 app.get('/urls/new', (req, res) => {
-  const { userID } = reqContainer(req);
-  if (!userID) {
-    return res.redirect('/login');
+  const userID = req.session.user_id;
+  if (!users[userID]) {
+    res.redirect('/login');
   } else {
     renderTemplateVariables(req, res, urlDatabase, users, 'urls_new');
   };
 });
 
 app.get('/register', (req, res) => {
-  renderTemplateVariables(req, res, urlDatabase, users, 'register');
+  if (!loginCheck(req)) {
+    renderTemplateVariables(req, res, urlDatabase, users, 'register');
+  } else {
+    res.redirect('/urls');
+  };
 });
 
 app.get('/login', (req, res) => {
@@ -41,34 +48,36 @@ app.get('/login', (req, res) => {
 });
 
 app.get('/urls/:shortURL', (req, res) => {
-  const { shortURL, userID } = reqContainer(req);
-  const longURL = urlDatabase[shortURL].longURL;
-  let templateVars = {
-    shortURL,
-    longURL,
-    user: users[userID]
-  }
-  res.render('urls_show', templateVars);
+  if (!loginCheck(req)) {
+    res.redirect('/login');
+  } else {
+    const userID = req.session.user_id;
+    const { shortURL } = shortLongURLReq(req);
+    const longURL = urlDatabase[shortURL].longURL;
+    let templateVars = { shortURL, longURL, user: users[userID] };
+    res.render('urls_show', templateVars);
+  };
 });
 
 app.get('/u/:shortURL', (req, res) => {
-  const { shortURL } = reqContainer(req);
+  const { shortURL } = shortLongURLReq(req);
   const longURL = urlDatabase[shortURL].longURL;
   res.redirect(longURL);
 });
 
 app.post('/urls', (req, res) => {
-  const { userID, longURL } = reqContainer(req);
-  const genShortURL = generateRandomString();
-  urlDatabase[genShortURL] = { longURL, userID };
-  res.redirect(`/urls/${genShortURL}`);
+  const userID = req.session.user_id;
+  const { longURL } = shortLongURLReq(req);
+  const generatedShortURL = generateRandomString();
+  urlDatabase[generatedShortURL] = { longURL, userID };
+  res.redirect(`/urls/${generatedShortURL}`);
 });
 
 app.post('/login', (req, res) => {
-  const { email, password } = reqContainer(req);
+  const { email, password } = emailPasswordReq(req);
   const user = getUserByEmail(users, email);
   if (!user) {
-    return res.status(403).send('email not found');
+    res.status(403).send('email not found');
   } else if (!bcrypt.compareSync(password, user.cryptPassword)) {
     return res.status(403).send('password does not match');
   } else {
@@ -81,7 +90,7 @@ app.post('/logout', (req, res) => {
 });
 
 app.post('/register', (req, res) => {
-  const { email, password } = reqContainer(req);
+  const { email, password } = emailPasswordReq(req);
   if (email === '') {
     return res.status(400).send('enter email');
   } else if (getUserByEmail(users, email)) {
@@ -94,8 +103,18 @@ app.post('/register', (req, res) => {
   };
 });
 
+app.post('/urls/:shortURL', (req, res) => {
+  if (!loginCheck(req)) {
+    res.redirect('/login');
+  } else {
+    const { shortURL } = shortLongURLReq(req);
+    res.redirect(`/urls/${shortURL}`); 
+  };
+});
+
 app.post('/urls/:shortURL/delete', (req, res) => {
-  const { shortURL, userID } = reqContainer(req);
+  const { shortURL } = shortLongURLReq(req);
+  const userID = req.session.user_id;
   const user = urlDatabase[shortURL].userID;
   if (userID === user) {
     delete urlDatabase[shortURL];
@@ -105,13 +124,8 @@ app.post('/urls/:shortURL/delete', (req, res) => {
   };
 });
 
-app.post('/urls/:shortURL', (req, res) => {
-  const { shortURL } = reqContainer(req);
-  res.redirect(`/urls/${shortURL}`); 
-});
-
 app.post('/urls/:shortURL/edit', (req, res) => {
-  const { shortURL } = reqContainer(req);
+  const { shortURL } = shortLongURLReq(req);
   const updatedURL = req.body.updatedURL;
   urlDatabase[shortURL].longURL = updatedURL;
   res.redirect('/urls'); 
--- helpers.js
+++ helpers.js
@@ -3,8 +3,8 @@ const getUserByEmail = (usersObject, enteredUserEmail) => {
   for (let user in usersObject) {
     if (usersObject[user].email === enteredUserEmail) {
       return usersObject[user];
-    }
-  }
+    };
+  };
   return false;
 };
 //Returns urls which the user entered
@@ -26,25 +26,34 @@ const generateRandomString = () => {
 //Renders template variables for entered site
 const renderTemplateVariables = (req, res, urlDatabase, usersObject, site) => {
   const userID = req.session.user_id;
-  let templateVars = {
-    urls: urlDatabase, 
-    user: usersObject[userID]
-  };
+  let templateVars = { urls: urlDatabase, user: usersObject[userID] };
   res.render(`${site}`, templateVars);
 };
 //Removes all session cookies from browser then redirects to /urls
 const eradicateCookies = (req, res) => {
   req.session = null;
-  res.redirect('urls');
+  res.redirect('/urls');
 };
 //Creates hashed cookie and redirects to /urls
 const creatCookie = (req, res, userIdentity) => {
   req.session.user_id = userIdentity;
   res.redirect('urls');
 };
-//Returns: email, password, userID, shortURL, longURL
-const reqContainer = (req) => {
-  return { email: req.body.email, password: req.body.password, userID: req.session.user_id, shortURL: req.params.shortURL, longURL: req.body.longURL };
+//Returns: email, password
+const  emailPasswordReq = (req) => {
+  return { email: req.body.email, password: req.body.password };
+};
+//Returns: shortURL, longURL, userID
+const shortLongURLReq = (req) => {
+ return { shortURL: req.params.shortURL, longURL: req.body.longURL };
+};
+
+const loginCheck = (req) => {
+  const userID = req.session.user_id;
+  if (userID) {
+    return true;
+  }
+  return false;
 };
 
 module.exports = { 
@@ -54,5 +63,7 @@ module.exports = {
   renderTemplateVariables, 
   eradicateCookies, 
   creatCookie, 
-  reqContainer
+  emailPasswordReq,
+  shortLongURLReq,
+  loginCheck
 };
